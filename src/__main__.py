ba74c62e8babca2f183fe2313288a827c48425e7
FedericoGarza <fede.garza.ramirez@gmail.com>
2024-09-21 04:29:29 +0000

    feat: add read data command

--- src/__main__.py
+++ src/__main__.py
@@ -1,2 +1,31 @@
-def my_awesome_function():
-    print("hey!")
+import os
+
+import pandas as pd
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def read_data():
+    username = os.getenv("TIMESCALE_USERNAME")
+    password = os.getenv("TIMESCALE_PASSWORD")
+    host = os.getenv("TIMESCALE_HOST")
+    port = os.getenv("TIMESCALE_PORT")
+    dbname = os.getenv("TIMESCALE_DBNAME")
+    CONNECTION = f"postgresql://{username}:{password}@{host}:{port}/{dbname}"
+    with psycopg2.connect(CONNECTION) as conn:
+        df = pd.read_sql_query(
+            """
+            SELECT symbol, bucket as date, open, high, low, close
+            FROM one_day_candle
+            ORDER BY symbol, bucket DESC
+            """,
+            conn,
+        )
+    return df
+
+
+if __name__ == "__main__":
+    df = read_data()
+    print(df.head())
