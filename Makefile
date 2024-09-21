b867163c4c3355d40d7a132e471fd4affdb9e95f
FedericoGarza <fede.garza.ramirez@gmail.com>
2024-09-21 03:55:03 +0000

    fix: update to finance use case

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 download_data:
 	@mkdir -p data
-	@wget https://assets.timescale.com/docs/downloads/metrics.csv.gz -O data/metrics.csv.gz
-	@gzip -dc metrics.csv.gz > data/metrics.csv
+	@wget https://assets.timescale.com/docs/downloads/get-started/real_time_stock_data.zip -O data/real_time_stock_data.zip 
+	@unzip data/real_time_stock_data.zip -d data
 
 populate_timescale: .require-project-id download_data 
-	@psql -d ${project_id} -f sql/create_metrics_table.sql
-
+	@psql -d ${project_id} -f sql/create_tables.sql
 
 .require-project-id:
 ifndef project_id
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,14 @@
-CREATE TABLE "metrics" (
-    created timestamp with time zone default now() not null,
-    type_id integer not null,
-    value double precision not null
+CREATE TABLE stocks_real_time (
+  time TIMESTAMPTZ NOT NULL,
+  symbol TEXT NOT NULL,
+  price DOUBLE PRECISION NULL,
+  day_volume INT NULL
 );
-SELECT create_hypertable('metrics', 'created');
-\COPY metrics FROM 'data/metrics.csv' CSV;
-SELECT * FROM metrics LIMIT 5;
+SELECT create_hypertable('stocks_real_time', by_range('time'));
+CREATE INDEX ix_symbol_time ON stocks_real_time (symbol, time DESC);
+CREATE TABLE company (
+  symbol TEXT NOT NULL,
+  name TEXT NOT NULL
+);
+\COPY stocks_real_time from './data/tutorial_sample_tick.csv' DELIMITER ',' CSV HEADER;
+\COPY company from './data/tutorial_sample_company.csv' DELIMITER ',' CSV HEADER;
