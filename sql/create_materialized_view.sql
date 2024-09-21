1d491c540f1fced51248f370720ecd11f6cc5b36
FedericoGarza <fede.garza.ramirez@gmail.com>
2024-09-21 04:11:58 +0000

    feat: add create materialized vide

---
+++ sql/create_materialized_view.sql
@@ -0,0 +1,16 @@
+CREATE MATERIALIZED VIEW one_day_candle
+WITH (timescaledb.continuous) AS
+    SELECT
+        time_bucket('1 day', time) AS bucket,
+        symbol,
+        FIRST(price, time) AS "open",
+        MAX(price) AS high,
+        MIN(price) AS low,
+        LAST(price, time) AS "close",
+        LAST(day_volume, time) AS day_volume
+    FROM stocks_real_time
+    GROUP BY bucket, symbol;
+SELECT add_continuous_aggregate_policy('one_day_candle',
+    start_offset => INTERVAL '3 days',
+    end_offset => INTERVAL '1 day',
+    schedule_interval => INTERVAL '1 day');
